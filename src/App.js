3300641f61d9e33d78e8eb9fe64fd8d2226b78db
unknown <pedro@tecnocarcuritiba.com.br>
2022-09-16 14:07:02 -0300

    Terceiro commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {FiSettings} from 'react-icons/fi'
 import {TooltipComponent, tooltipComponent} from '@syncfusion/ej2-react-popups'
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
 import {Ecommerce, Line, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
+import { useStateContext } from './contexts/ContextProvider'
 import './App.css'
 
 
 const App = () => {
-    const activeMenu = true
+    
+    const {activeMenu} = useStateContext()
   return (
     
     <div>
