42d4a9b2abb55be25fe551c30e380bb6534a0205
Pedro <pedro@tecnocarcuritiba.com.br>
2022-09-18 19:28:00 -0300

    Finalizado ECommerce

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -9,19 +9,32 @@ const initialState = {
     notification: false,
 }
 
+
 export const ContextProvider = ({children}) => {
     const [activeMenu, setActiveMenu] = useState(true)
-    
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
+
+
     return (
         <StateContext.Provider
             value={({
                 activeMenu:activeMenu,
                 setActiveMenu,
+                isClicked, 
+                setIsClicked,
+                handleClick,
+                screenSize, 
+                setScreenSize
             })}
         >
             {children}
         </StateContext.Provider>
     )
+
+    
 }
 
 export const useStateContext = () => useContext(StateContext)
